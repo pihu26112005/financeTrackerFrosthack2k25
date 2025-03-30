8b7480a09bd8cd4086ed23629963b633b73b030f
mrhello291 <hoda.asif123@gmail.com>
2025-03-30 14:48:15 +0530

    added self-rag pipeline for client side

--- pathway/pathwayServer.py
+++ pathway/pathwayServer.py
@@ -4,9 +4,8 @@ from pathway.xpacks.llm import llms, parsers
 import os
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
-import json
-from langchain_huggingface import HuggingFaceEmbeddings
-# from langchain_community.embeddings import HuggingFaceEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
 # local = pw.io.fs.read(
@@ -16,53 +15,45 @@ from langchain_huggingface import HuggingFaceEmbeddings
 #     with_metadata=True,
 # )
 
-# gdrive = pw.io.gdrive.read(
-#     object_id="14cPcPF19g3LPGojMTRhoNCFTAx8sTV0a",
-#     service_user_credentials_file="../credentials.json"
-# )
+gdrive = pw.io.gdrive.read(
+    object_id=os.environ.get("GDRIVEFOLDERID"),
+    service_user_credentials_file="credentials.json",
+    file_name_pattern="*.txt",
+)
 
 files = gdrive
-
-unStructureParser = parsers.UnstructuredParser()
-unstructured_documents = files.select(texts=unStructureParser(pw.this.data))
-unstructured_documents = unstructured_documents.select(texts=strip_metadata(pw.this.texts))
-
-prompt = unstructured_documents.select(prompt=build_prompt_structure(pw.this.texts))
-
-
-
 
 # model_name = "BAAI/bge-small-en"
 # model_kwargs = {"device": "cpu"}
 # encode_kwargs = {"normalize_embeddings": True}
 
 
-# model_name = "sentence-transformers/all-mpnet-base-v2"
-# model_kwargs = {'device': 'cpu'}
-# encode_kwargs = {'normalize_embeddings': True}
-# embeddings = HuggingFaceEmbeddings(
-#     model_name=model_name,
-#     model_kwargs=model_kwargs,
-#     encode_kwargs=encode_kwargs
-# )
+model_name = "sentence-transformers/all-mpnet-base-v2"
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': True}
+embeddings = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs
+)
 
-# splitter = CharacterTextSplitter()
+splitter = CharacterTextSplitter()
 
-# parser = parsers.PypdfParser()
+# parser = parsers.DoclingParser()
 
-# host = "127.0.0.1"
-# port = 8666
+host = "127.0.0.1"
+port = 8666
 
-# server = VectorStoreServer.from_langchain_components(
-#     *[local, gdrive], embedder=embeddings, splitter=splitter, parser=parser
-# )
+server = VectorStoreServer.from_langchain_components(
+    gdrive, embedder=embeddings, splitter=splitter
+)
 
-# server.run_server(
-#     host=host,
-#     port=port,
-#     with_cache=True,
-#     cache_backend=pw.persistence.Backend.filesystem("./Cache"),
-#     # threaded=True
-# )
+server.run_server(
+    host=host,
+    port=port,
+    with_cache=True,
+    cache_backend=pw.persistence.Backend.filesystem("./Cache"),
+    # threaded=True
+)
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------
